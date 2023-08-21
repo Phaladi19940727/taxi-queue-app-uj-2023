93947f2b6454dddc19a2569de98ce6d850ad7e30
Phaladi19940727 <phaladistephina@gmail.com>
2023-08-21 16:16:14 +0200

    Phaladi

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -1,35 +1,50 @@
-document.addEventListener('alpine:init', () => {
-
-	Alpine.data('TaxiQueue', () => {
-
-		return {
-			version: 'no-api-1.0',
-			joinQueue() {
-
-			},
-			leaveQueue() {
-
-			},
-
-			joinTaxiQueue() {
-
-			},
-
-			queueLength() {
-
-			},
-
-			taxiQueueLength() {
-
-			},
-
-			taxiDepart() {
-
-			}
-		}
-
+document.addEventListener("alpine:init", () => {
+	Alpine.data("TaxiQueue", () => {
+	  return {
+		version: "no-api-1.0",
+		taxi_queue_count: 0,
+		queueLengthPassnger: 0,
+		queueLengthPassenger: 0,
+		queueLengthTaxi: 0,
+		taxiDepartPassenger: "",
+		TaxiDepartTaxi: "",
+  
+		joinQueue() {
+		  this.queueLengthPassnger += 1;
+		  (localStorage = localStorage["queueLengthPassnger"]),
+			this.passengerMessage;
+		},
+		leaveQueue() {
+		  if (this.queueLengthPassnger > 0) {
+			this.queueLengthPassnger -= 1;
+		  }
+		},
+  
+		joinTaxiQueue() {
+		  this.taxi_queue_count += 1;
+		  localStorage = localStorage["taxi_queue_count "];
+		},
+  
+		queueLength() {
+		  if (queueLengthPassnger == 12) {
+			return joinTaxiQueue();
+		  }
+		},
+  
+		taxiQueueLength() {},
+  
+		taxiDepart() {
+		  if (this.queueLengthPassnger >= 12) {
+			this.taxiDepartPassenger = this.queueLengthPassnger - 12;
+			this.TaxiDepartTaxi = this.taxi_queue_count - 1;
+	  this.queueLengthPassnger = this.taxiDepartPassenger;
+	  this.taxi_queue_count = this.TaxiDepartTaxi;
+		  } else{
+  
+	 return "Not enough passengers for taxi to depart"
+	}
+		},
+	  };
 	});
-
-});
-
-
+  });
+  
--- public/with-api/taxi-queue.api.js
+++ public/with-api/taxi-queue.api.js
@@ -1,43 +1,84 @@
-document.addEventListener('alpine:init', () => {
+return {
+    version: 'api-1.0',
+    queueLength: 0,
+    queueLen: 0,
+    taxiQueueLen: 0,
 
-	Alpine.data('TaxiQueue', () => {
+    init() {
+        this.queueLength()
+        this.taxiQueueLength()
+    },
 
-		return {
-			version: 'api-1.0',
-			queueLength: 0,
-			init() {
-				axios
-					.get('/api/passenger/queue')
-					.then(result => {
-						// an example API call
-						this.queueLength = result.data.queueCount
-					});
-			},
-			joinQueue() {
+    queueLength() {
+        axios
+            .get('/api/passenger/queue')
+            .then(result => {
+                // an example API call
+                this.queueLength = result.data.queueCount
+                console.log(result.data.queueCount)
+            });
+    },
+    joinQueue() {
 
-			},
-			leaveQueue() {
+    },
+    leaveQueue() {
 
-			},
+    joinQueue() {
+        axios
+            .post('/api/passenger/join')
+            .then((result) => {
+                console.log(result.data.message);
+            })
+    },
 
-			joinTaxiQueue() {
+    joinTaxiQueue() {
 
-			},
+    leaveQueue() {
+        axios
+            .post('/api/passenger/leave')
+            .then((result) => {
+                console.log(result.data.message);
+            })
+    },
 
-			queueLength() {
+    queueLength() {
 
-			},
+    joinTaxiQueue() {
+        axios
+            .post('/api/taxi/join')
+            .then((result) => {
+                console.log(result.data.message);
+            })
+    },
 
-			taxiQueueLength() {
+    taxiQueueLength() {
+        axios
+            .get('/api/taxi/queue')
+            .then((result) => {
+                this.taxiQueueLen = result.data.queueCount
+            })
+    },
 
-			},
+    queueLength() {
+        axios
+            .get('/api/passenger/queue')
+            .then((result) => {
+                this.queueLen = result.data.queueCount
+            })
+    },
 
-			taxiDepart() {
+    taxiDepart() {
 
-			}
-		}
-	});
+        axios
+            .post('/api/taxi/depart')
+            .then((result) => {
+                console.log(result.data.message);
+            })
+    }
+}
+});
 
 });
 
 
+});
